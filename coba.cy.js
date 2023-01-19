540d1f431b1aeda5676c4f9f914fbb0e94e4ac1a
syauqijufri <61929268+syauqijufri@users.noreply.github.com>
2023-01-19 20:37:41 +0700

    Delete coba.cy.js

--- coba.cy.js
+++
@@ -1,8 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://the-internet.herokuapp.com/login')
-    cy.get('#username').type("tomsmith")
-    cy.get('#password').type("SuperSecretPassword")
-  })
-})
-
